9f9018483d546e7682b4200f1355a4e5ea07be52
Daniel Garay <garay.daniel@protonmail.com>
2022-01-30 15:45:26 -0500

    move folders for netlify

---
+++ build/precache-manifest.1004460e1847fd3e0c607ee898288850.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "125c0e810edad36b138b",
+    "url": "/static/css/main.3babc2b3.chunk.css"
+  },
+  {
+    "revision": "125c0e810edad36b138b",
+    "url": "/static/js/main.bfea6dd8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "487a8ad15c776904c791",
+    "url": "/static/css/2.6546bfff.chunk.css"
+  },
+  {
+    "revision": "487a8ad15c776904c791",
+    "url": "/static/js/2.1187da8f.chunk.js"
+  },
+  {
+    "revision": "ba64bdb9655fb488674ad3e1be6e13e9",
+    "url": "/index.html"
+  }
+];
